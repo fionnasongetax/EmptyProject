5c54f00e71b70cc2e0b6efb7fa5b1d009a9d48e7
fionnasongetax <fionna.song@etax.com.au>
2017-11-02 14:17:47 +1000

    Added ThisIsALibrary

---
+++ ThisIsALibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThisIsALibrary
+{
+    public class Class1
+    {
+    }
+}
